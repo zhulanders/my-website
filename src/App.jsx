eebe0426e9a0c1856fb0af7a559f584592d30145
Joshua Zhu <jzhu2003@gmail.com>
2025-05-27 23:45:00 -0700

    navbar complete

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from "./sections/Navbar.jsx";
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+        <Navbar />
+    </main>
+  )
+}
+export default App
